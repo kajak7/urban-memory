d3be3044947ede6d0eedac7a037bc9c77305dd66
kajak7 <37246524+kajak7@users.noreply.github.com>
2018-05-15 09:44:22 +0200

    Add files via upload

--- dodaj.php
+++ dodaj.php
@@ -1,17 +1,94 @@
-<!doctype html>
+﻿<!doctype html>
 <html>
   <head>
-    <title>Dodawanie książki w bazie ksiegarnia_internetowa</title>
+    <link rel="Stylesheet" type="text/css" href="style.css" />
+    <title>Ankieta dotycząca wystąpienia ryzyka zawału miesniowego</title>
     <meta charset="utf-8" />
   </head>
-  <body>
-    <h1>Dodawanie książki w bazie ksiegarnia_internetowa</h1>
+     <body style=" background-attachment: fixed; background-image: url(ankieta.jpg); 
+	 background-repeat: no-repeat; background-size: cover ">
+  
+    <h1>Ankieta dotycząca wystąpienia ryzyka zawału miesniowego</h1>
     <form action="wynik.php" method="post">
-      <input type="text" name="isbn" /> ISBN<br /><br />
-      <input type="text" name="autor" /> Autor<br /><br />
-      <input type="text" name="tytul" /> Tytuł<br /><br />
-      <input type="text" name="cena" /> Cena<br /><br />
-      <input type="submit" name="dodaj" value="Dodaj" />
+	
+	
+	<h2>1. Płeć</h2>
+       <input type="radio" name="plec" value="K" /> Kobieta <br /><br />
+	   <input type="radio" name="plec" value="M" /> Mężczyzna<br /><br />
+	
+    <h2>2. Pana/Pani wzrost [cm]?</h2>
+      <input type="text" name="wzrost" /> <br /><br />
+	  
+	<h2>3. Pana/Pani wiek?</h2>
+      <input type="text" name="wiek" /> <br /><br />
+	  
+	  	<h2>4. Pana/Pani waga?</h2>
+      <input type="text" name="waga" /> <br /><br />
+	
+
+
+	<h2>5. Jak często Pan/Pani uprawia jakąkolwiek aktywność fizyczną?</h2>
+	
+    <input type="radio" name="aktywnosc" value="5" /> Codziennie <br /><br />
+   	<input type="radio" name="aktywnosc" value="4" /> 3 lub wiecej razy w tygodniu<br /><br />
+	<input type="radio" name="aktywnosc" value="3" /> 1-2 razy w tygodniu <br /><br />
+	<input type="radio" name="aktywnosc" value="2" /> 1-2 razy w miesiącu <br /><br />
+	<input type="radio" name="aktywnosc" value="1" /> Rzadziej <br /><br />
+	
+	
+	<h2>6. Jaki jest tryb Pana/Pani pracy?</h2>
+    <input type="radio" name="tryb" value="4" /> Umysłowa <br /><br />
+   	<input type="radio" name="tryb" value="3" /> Fizyczna<br /><br />
+	<input type="radio" name="tryb" value="3" /> Nie pracuję<br /><br />
+
+	<h2>7. Jak często jest Pan/Pani poddawana stresującym sytuacjom w domu lub w pracy?</h2>
+      <input type="radio" name="stres" value="4" /> Codziennie <br /><br />
+	   <input type="radio" name="stres" value="3" /> Kilka razy w tygodniu <br /><br />
+	    <input type="radio" name="stres" value="2" /> Kilka razy w miesiącu <br /><br />
+		 <input type="radio" name="stres" value="1" /> Rzadko <br /><br />
+		 
+		 <h2>8. Czy pali Pan/Pani papierosy?</h2>
+      <input type="radio" name="nikotyna" value="4" /> Tak <br /><br />
+	   <input type="radio" name="nikotyna" value="3" /> Nie <br /><br />
+	    <input type="radio" name="nikotyna" value="2" /> Paliłem/am w przeszłości <br /><br />
+		 <input type="radio" name="nikotyna" value="1" /> Jestem biernym palaczem <br /><br />
+		 
+		 <h2>9. Jak często pije Pan/Pani alkohol?</h2>
+      <input type="radio" name="alkohol" value="5" /> Codziennie<br /><br />
+	   <input type="radio" name="alkohol" value="4" /> 1-2 razy w tygodniu <br /><br />
+	    <input type="radio" name="alkohol" value="3" /> Kilka razy w miesiącu <br /><br />
+		 <input type="radio" name="alkohol" value="2" /> Rzadziej <br /><br />
+		 <input type="radio" name="alkohol" value="1" /> Nie piję <br /><br />
+
+		 <h2>10. Jakie ma Pan/Pani ciśnienie tętnicze krwi?</h2>
+      <input type="radio" name="cisnienie" value="4"  /> Prawidłowe/prawidłowe wysokie (<120/80 - 140/90)<br /><br />
+	   <input type="radio" name="cisnienie" value="3" /> Nadciśnienie łagodne/umiarkowane (140/90 - 170/95) <br /><br />
+	    <input type="radio" name="cisnienie" value="2" /> Nadciśnienie ciężkie ( powyżej 170/100) <br /><br />
+		 <input type="radio" name="cisnienie" value="1" /> Nie wiem<br /><br />
+		
+		 <h2>11. Jaki ma Pan/Pani poziom cholesterolu (całkowitego) we krwi?</h2>
+        <input type="radio" name="cholesterol" value="4" /> Optymalny - poniżej 200 mg/dl<br /><br />
+	    <input type="radio" name="cholesterol" value="3" /> Podwyższony  <br /><br />
+	    <input type="radio" name="cholesterol" value="2" /> Nadciśnienie ciężkie ( powyżej 170/100) <br /><br />
+		<input type="radio" name="cholesterol" value="1" />Nie wiem<br /><br />
+		
+		<h2>12. Czy choruje Pan/Pani na którąś z poniższych chorób? </h2>
+        <input type="checkbox" name="choroba1" value="1" /> Miażdzyca<br /><br />
+	    <input type="checkbox" name="choroba2" value="1" /> Choroba wieńcowa<br /><br />
+	    <input type="checkbox" name="choroba3" value="1" /> Cukrzyca <br /><br />
+		<input type="checkbox" name="choroba4" value="1" /> Zaburzenia rytmu serca<br /><br />
+	    <input type="checkbox" name="choroba5" value="1" /> Nadciśnienie tętnicze<br /><br />
+	    <input type="checkbox" name="choroba6" value="1" /> Nie choruje<br /><br />
+		
+		 <h2>13. Czy ktoś u Pana/Pani w rodzinie wśród najbliższych chorował na wyżej wymienione choroby?</h2>
+        <input type="radio" name="choroby_rodzina" value="Tak" /> Tak<br /><br />
+	    <input type="radio" name="choroby_rodzina" value="Nie" /> Nie <br /><br />
+	    <input type="radio" name="choroby_rodzina" value="Nie wiem" /> Nie wiem <br /><br />
+		
+        <input type="submit" name="wyslij" value="Wyślij" />
+	    <input type="reset" name="wyczyść" value="Wyczyść" />
+	    <br /><br />
+	   
     </form>
   </body>
 </html>
--- wynik.php
+++ wynik.php
@@ -1,40 +1,143 @@
-<!doctype html>
+﻿<!doctype html>
 <html>
-  <head>
-    <title>Wyniki dodania książki</title>
-    <meta charset="utf-8" />
-  </head>
-  <body>
-    <h1>Wyniki dodania książki</h1>
-    <?php
-      $isbn = trim($_POST['isbn']);
-      $autor = trim($_POST['autor']);
-      $tytul = trim($_POST['tytul']);
-      $cena = trim($_POST['cena']);
-      if (!$isbn || !$autor || !$tytul || !$cena)
-      {
-        echo 'Brak wszystkich danych, wróć do poprzedniej strony i spóbuj ponownie!';
+    <head>
+        <link rel="Stylesheet" type="text/css" href="style.css" />
+        <title>Wyniki dodania ankiety</title>
+        <meta charset="utf-8" />
+    </head>
+    <body style=" background-attachment: fixed; background-image: url(ankieta2.jpg); 
+          background-repeat: no-repeat; background-size: cover ">
+
+        <?php
+        error_reporting(E_ALL ^ E_NOTICE);
+        $plec = trim($_POST['plec']);
+        $wzrost = trim($_POST['wzrost']);
+        $wiek = trim($_POST['wiek']);
+        $waga = trim($_POST['waga']);
+        $aktywnosc = trim($_POST['aktywnosc']);
+        $tryb= trim($_POST['tryb']);
+        $stres = trim($_POST['stres']);
+        $nikotyna = trim($_POST['nikotyna']);
+        $alkohol= trim($_POST['alkohol']);
+        $cisnienie = trim($_POST['cisnienie']);
+        $cholesterol = trim($_POST['cholesterol']);
+        $choroba1 = $_POST['choroba1'];
+        $choroba2 = $_POST['choroba2'];
+        $choroba3 = $_POST['choroba3'];
+        $choroba4 = $_POST['choroba4'];
+        $choroba5 = $_POST['choroba5'];
+        $choroba6 = $_POST['choroba6'];
+
+        $choroby_rodzina= trim($_POST['choroby_rodzina']);
+        if (!($plec) || !$wzrost || !$wiek|| !$waga|| !$aktywnosc || !$tryb || !$stres || !$nikotyna || !$alkohol || !$cisnienie || !$cholesterol || 
+        !($choroba1 || $choroba2 || $choroba3 || $choroba4 || $choroba5 || $choroba6) || !$choroby_rodzina)
+        {
+
+        ?>
+        <h1>Brak wszystkich danych, wróć do poprzedniej <br>
+        strony i spóbuj ponownie! :)</h1>
+        <?php
+
         exit;
-      }
-      if (!get_magic_quotes_gpc())
-      {
-        $isbn = addslashes($isbn);
-        $autor = addslashes($autor);
-        $tytul = addslashes($tytul);
-        $cena = doubleval($cena);
-      }
-      @ $db = new mysqli('localhost','root','admin123','dane_ankietowane');
-      
-      if (mysqli_connect_errno())
-      {
-        echo 'Połączenie z bazą nie powiodło się. Spóbuj ponownie';
-        exit;
-      }
-      $db->query('SET NAMES utf8');
-      $db->query('SET CHARACTER_SET utf8_unicode_ci');
-      $zapytanie = "insert into ksiazki values ('".$isbn."', '".$autor."', '".$tytul."', '".$cena."')";
-      $wynik = $db->query($zapytanie);
-      if ($wynik) echo 'Liczba książek dodanych do bazy:'.$db->affected_rows;
-    ?> 
-  </body>
+        }
+
+        //wyłączenie magicznych znaków - \
+        if (!get_magic_quotes_gpc())
+        {
+        $plec = addslashes($plec);
+        $wzrost = addslashes($wzrost);
+        $wiek = addslashes($wiek);
+        $waga = addslashes($waga);
+        $aktywnosc = doubleval($aktywnosc);
+        $tryb = addslashes($tryb);
+        $stres = addslashes($stres);
+        $nikotyna = doubleval($nikotyna);
+        $alkohol = addslashes($alkohol);
+        $cisnienie = addslashes($cisnienie);
+        $cholesterol = doubleval($cholesterol);
+        $choroba1 = addslashes($choroba1);
+        $choroba2 = addslashes($choroba2);
+        $choroba3 = addslashes($choroba3);
+        $choroba4 = addslashes($choroba4);
+        $choroba5 = addslashes($choroba5);
+        $choroba6 = addslashes($choroba6);
+        $choroby_rodzina = addslashes($choroby_rodzina);
+        }
+
+        //łaczenie z baza
+        $db = new PDO('mysql:host=localhost;dbname=dane_ankietowane','root','new_password',array(PDO::ATTR_PERSISTENT => true));
+
+
+
+        ?>
+        <h1>Dziekujemy za wypełnienie ankiety! :)</h1>
+        <h2>Dzieki Tobie nasza baza danych sie powieksza,<br>
+            przez co wyniki beda bardziej wiarygodne!</h2>
+        <?php
+
+
+        if($db === false){
+        try{
+		$db = new PDO('mysql:host=localhost;dbname=dane_ankietowane', 'root', 'new_password');
+        }
+        catch (PDOException $e)
+        {
+        print "Błąd połączenia z bazą!: ".$e -> getMessage()."<br/>";
+        die();
+        }
+        }
+
+        //szkielet zapytania
+        $statement = $db->prepare("INSERT INTO ankietowani (plec, wiek, wzrost,waga,aktywnosc,tryb)
+        VALUES  (:plec, :wiek, :wzrost, :waga, :aktywnosc, :tryb)");
+
+        //przypisujemy (bindujemy) wartość zmiennej $plec do placeholdera :plec typu Stringa
+        //informując tym samym bazę jakiego typu wartości się spodziewać
+		
+        $statement->bindValue(':plec', $_POST['plec'], PDO::PARAM_STR);
+        $statement->bindValue(':wiek',  $_POST['wiek'], PDO::PARAM_INT);
+        $statement->bindValue(':wzrost', (float) $_POST['wzrost'], PDO::PARAM_STR);
+        $statement->bindValue(':waga', (float) $_POST['waga'], PDO::PARAM_STR);
+        $statement->bindValue(':aktywnosc', $_POST['aktywnosc'], PDO::PARAM_INT);
+        $statement->bindValue(':tryb',  $_POST['tryb'], PDO::PARAM_INT);
+		
+        //wysylamy całość do bazy
+        $statement->execute();
+
+        $statement1 = $db->prepare("INSERT INTO choroby (cisnienie, cholesterol,stres,choroby_rodzina) 
+        VALUES (:cisnienie,:cholesterol, :stres, :choroby_rodzina)");
+
+        $statement1->bindValue(':cisnienie', $_POST['cisnienie'], PDO::PARAM_INT);
+        $statement1->bindValue(':cholesterol',  $_POST['cholesterol'], PDO::PARAM_INT);
+        $statement1->bindValue(':stres',  $_POST['stres'], PDO::PARAM_INT);
+        $statement1->bindValue(':choroby_rodzina',  $_POST['choroby_rodzina'], PDO::PARAM_STR);
+        $statement1->execute();
+
+
+        $statement2 = $db->prepare("INSERT INTO uzaleznienia (nikotyna,alkohol) VALUES (:nikotyna, :alkohol)");
+
+        $statement2->bindValue(':nikotyna', $_POST['nikotyna'], PDO::PARAM_INT);
+        $statement2->bindValue(':alkohol',  $_POST['alkohol'], PDO::PARAM_INT);
+        $statement2->execute();
+
+
+        $statement3 = $db->prepare("INSERT INTO choroby_posiadane (miazdzyca,choroba_wiencowa,cukrzyca,zaburzenie_rytmu_serca,nadcisnienie_tetnicze,nie_choruje) 
+        VALUES (:choroba1, :choroba2, :choroba3, :choroba4, :choroba5, :choroba6)");
+
+        $statement3->bindValue(':choroba1', (int) $_POST['choroba1'], PDO::PARAM_STR);
+        $statement3->bindValue(':choroba2', (int) $_POST['choroba2'], PDO::PARAM_INT);
+        $statement3->bindValue(':choroba3', (int) $_POST['choroba3'], PDO::PARAM_INT);
+        $statement3->bindValue(':choroba4', (int) $_POST['choroba4'], PDO::PARAM_INT);
+        $statement3->bindValue(':choroba5', (int) $_POST['choroba5'], PDO::PARAM_INT);
+        $statement3->bindValue(':choroba6', (int) $_POST['choroba6'], PDO::PARAM_INT);
+        $statement3->execute();
+
+        //zwalniamy kursor
+        $statement->closeCursor();
+        $statement1->closeCursor();
+        $statement2->closeCursor();
+        $statement3->closeCursor();
+
+        ?> 
+    </body>
 </html>
